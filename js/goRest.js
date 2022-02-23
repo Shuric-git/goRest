9b16a1c053f90e47f16a36472885826970bc773a
shuric-git <ulme@rambler.ru>
2022-02-23 23:55:29 +0300

    done with core mechanic, start to stylize

--- js/goRest.js
+++ js/goRest.js
@@ -1,85 +1,145 @@
-async function loadPostsList() {
-    const response = await fetch(`https://gorest.co.in/public-api/posts${window.location.search}`)
-    const responseData = await response.json();
+function createPostsList() {
     
-    const postsList = document.createElement('ul')
-    
-    for ( let i = 0; i < responseData.data.length; i++) {
-        const post = document.createElement('li');
-        post.textContent = [responseData.data[i].id]
-        postsList.append(post)
-    }
-    
-    // console.log(postsList)
+    const postsList = document.createElement('ul') 
+    postsList.classList.add('list-group')
+
     return postsList
 }
 
-// document.addEventListener('DOMContentLoaded', async() => {
-    const container = document.getElementById('container')
+function createPageList() {
 
-    async function loadPageList() {
-        const response = await fetch('https://gorest.co.in/public-api/posts');
-        const responseData = await response.json();
+    const pageNav = document.createElement('nav');
+    const pagesList = document.createElement('ul');
+    pageNav.classList.add('Page', 'navigation', 'example');
+    pagesList.classList.add('pagination');
+    pagesList.style.display = 'flex';
+    pagesList.style.listStyle = 'none';
+
+    pageNav.append(pagesList)
+
+    return {
+        pageNav,
+        pagesList
+    };
+}
+
+function createPostTitle() {
+    const title = document.createElement('h1');
+
+    return title
+}
+
+function createPostBody() {
+    const body = document.createElement('p')
+
+    return body
+}
+
+function createComment() {
+    const comment = document.createElement('ul');
+
+    return comment
+}
+
+async function loadPostList(container) {
+    const pageList = createPageList();
+    const postList = createPostsList();
+
+    const response = await fetch(`https://gorest.co.in/public-api/posts${window.location.search}`)
+    const responseData = await response.json();
 
         
-        const pageList = document.createElement('ul')
-        pageList.style.display = 'flex';
-        pageList.style.listStyle = 'none';
-        container.append(pageList)
+    for ( let i = 0; i < responseData.data.length; i++) {
+        const post = document.createElement('li');
+        post.classList.add('list-group-item', 'list-group-item-action')
+        post.textContent = [responseData.data[i].id];
+        postList.append(post);
+    }
+    
+    container.append(pageList.pageNav);
+    console.log(pageList.pagesList)
 
-        pageList.id = 'pageList'
-        // for ( let head of Object.values(responseData)) {
-            for ( let i = 0; i < responseData.meta.pagination.pages; i++ ) {
+    // pageList.id = 'pageList'
+        for ( let i = 0; i < responseData.meta.pagination.pages; i++ ) {
 
-                const pageLink = document.createElement('li')
-                pageLink.id = 'link'
-                pageLink.textContent = [i + 1]
-                pageLink.style.margin = '1px';
-                // pageLink.href = `file:///D:/HTML/SkillBox/goRest/goRest.html?page=${link.textContent}`
-                
-                pageList.append(pageLink)
-                
-            }
-        // }
+            const pageLink = document.createElement('li')
+            const pageInner = document.createElement('a');
+            pageLink.classList.add('page-item')
+            pageInner.id = 'link';
+            pageInner.textContent = [i + 1];
+            // pageInner.style.margin = '1px';
+            pageInner.classList.add('page-link');
+            
+            pageLink.append(pageInner)
+            pageList.pagesList.append(pageLink);
+        }
+
+        pageList.pagesList.addEventListener('click', async (e) => {
+        e.preventDefault();
+        if ( e.target.tagName === 'A' ) {
+            window.location = `goRest.html?page=${e.target.textContent}`
+        }
+    })
+
+    const pageParams = new URLSearchParams(window.location.search); 
+    pageParams.get('page'); 
+
+    postList.addEventListener('click', (e) => {
+        e.preventDefault();
+        if ( e.target.tagName === 'LI' ) {
+            window.location = `goRestPost.html?page=${pageParams.get('page')}&posts=${e.target.textContent}`
+        }
+    })
+    
+    container.append(postList);
+
+    // console.log(responseData.data)
+}
+
+async function loadPost(container) {
+    const pageParams = new URLSearchParams(window.location.search); 
+    pageParams.get('posts'); 
+    const response = await fetch(`https://gorest.co.in/public-api/posts/${pageParams.get('posts')}`)
+    const responseData = await response.json();
+
+    const postTitle = createPostTitle();
+    const postBody = createPostBody();
+
+    postTitle.innerText = responseData.data.title
+    postTitle.classList.add('display-1')
+
+    postBody.innerText = responseData.data.body
+
+    container.append(postTitle)
+    container.append(postBody)
+
+    const commentPageParams = new URLSearchParams(window.location.search); 
+    pageParams.get('posts'); 
+    const commentResponse = await fetch(`https://gorest.co.in/public-api/comments?post_id=${pageParams.get('posts')}`)
+    const commentResponseData = await commentResponse.json();
+
+    for (let i = 0; i < commentResponseData.data.length; i++) {
+    const comm = createComment();
+            const commName = document.createElement('li');
+            const commBody = document.createElement('li');
+
+            commName.textContent = [commentResponseData.data[i].name];
+            commBody.textContent = [commentResponseData.data[i].body];
+
+            comm.append(commName);
+            comm.append(commBody);
+
         
-        // const link = document.getElementById('link')
-        // console.log(link.textContent)
-        pageList.addEventListener('click', async (e) => {
-            e.preventDefault();
-            if ( e.target.tagName === 'LI' ) {
-                window.location = `goRest.html?page=${e.target.textContent}`
-            }
-        })
+        console.log(commentResponseData.data[i])
+    container.append(comm)
     }
 
-async function createPostList(container) {
-    const postList = loadPostsList()
-    
-    container.append(postList)
-    console.log(postList)
-
-    // postList.addEventListener('click', (e) => {
-    //     e.preventDefault();
-    //     if ( e.target.tagName === 'LI' ) {
-    //         window.location = `goRestPost.html?page=${e.target.textContent}`
-    //     }
-    // })
 
 }
 
 
-// });
 
 
-    
-    
-// let link = document.createElement('a')
-
-// link.textContent = 'я ссылка'
-
-// link.href = 'https://gorest.co.in/public/v2/posts?page=98'
-
-// container.append(link)
 
 
 
